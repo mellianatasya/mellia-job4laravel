eb707a3582fb03ce14eb85ffefcd1253107cba6e
mellianatasya <mellianatasya731@gmail.com>
2025-03-10 14:14:15 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+            <div class="card-header">
+                <h1>Profile Saya</h1>
+            </div>
+            <div class="card-body">
+                <img src="profile.png" alt="Profile Picture" width="150">
+                <h3>Mellia Natasya</h3>
+                <p>Email: mellianatasya731@gmail.com</p>
+                <p>Bis: Pelajar SMK Negeri 1 Sayunng</p>
+                <h5>Skills</h5>
+                <ul>
+                    <li>Scroll</li>
+                    <li>Masak Mie</li>
+                    <li>Membuat Teh</li>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
